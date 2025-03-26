1568c324426d21b6ce3f2a9c7358835997d5db44
Degisew <jegisew21@gmail.com>
2025-03-26 22:36:00 +0300

    fix: Custom user model fields

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 from apps.account.views import (
     RoleViewSet,
     UserViewSet,
-    # CompanyProfileViewSet,
+    USerProfileViewSet,
     PasswordChangeViewSet
 )
 
@@ -10,7 +10,7 @@ router = DefaultRouter()
 
 router.register('roles', RoleViewSet, basename='roles')
 router.register('users', UserViewSet, 'users')
-# router.register('company-profile', CompanyProfileViewSet, 'company-profile')
+router.register('profile', USerProfileViewSet, 'company-profile')
 router.register('change-password', PasswordChangeViewSet, 'change-password')
 
 urlpatterns = router.urls
