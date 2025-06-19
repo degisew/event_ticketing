5c6cf2dc173d07b23558fe05077ff831a4a0ef77
Degisew <jegisew21@gmail.com>
2025-06-19 23:23:02 +0300

    ref: model design and relationships

--- apps/event/models.py
+++ apps/event/models.py
@@ -7,19 +7,15 @@ from apps.core.models import AbstractBaseModel, DataLookup
 class Event(AbstractBaseModel):
     name = models.CharField(verbose_name=_("Event Name"), max_length=255)
 
-    code = models.CharField(verbose_name=_("Event Code"), max_length=50, unique=True)
+    code = models.CharField(verbose_name=_(
+        "Event Code"), max_length=50, unique=True
+    )
 
     organizer = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.RESTRICT,
         related_name="events",
         verbose_name=_("Event Organizer")
-    )
-
-    ticket_price = models.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-        verbose_name=_("Ticket Prcie")
     )
 
     description = models.TextField(verbose_name=_("Event Description"))
@@ -36,14 +32,10 @@ class Event(AbstractBaseModel):
 
     is_active = models.BooleanField(verbose_name=_("Is Active"), default=True)
 
-    capacity = models.PositiveIntegerField(verbose_name=_("Capacity"), default=0)
-
-    available_seats = models.PositiveIntegerField(
-        verbose_name=_("Available Seats"), default=0
-    )
+    capacity = models.PositiveIntegerField(
+        verbose_name=_("Capacity"), default=0)
 
     class Meta:
-        ordering = ["-created_at"]
         verbose_name = _("Event")
         verbose_name_plural = _("Events")
         db_table = "events"
@@ -53,20 +45,78 @@ class Event(AbstractBaseModel):
             )
         ]
 
+    def __str__(self) -> str:
+        return self.name
+
+
+class TicketType(AbstractBaseModel):
+    """
+    Represents a reusable ticket type(e.g., VIP, Early Bird, General Admission)
+    tailored to organizer needs.
+
+    Instead of storing ticket type names and descriptions repeatedly
+    across events, the fields 'category' references a centralized
+    lookup (via `DataLookup`) that holds common ticket type definitions.
+    This promotes consistency, reduces redundancy, and helps prevent
+    human errors (e.g., typos or naming variations like 'VIP' vs 'V.I.P').
+
+    Attributes:
+        category (ForeignKey): A reference to a predefined
+        ticket type entry in the DataLookup table.
+
+    Inherits From:
+        AbstractBaseModel: A base model providing common
+        fields like 'id', 'created_at', 'updated_at', etc.
+    """
+
+    category = models.ForeignKey(
+        DataLookup,
+        on_delete=models.CASCADE,
+        limit_choices_to={"type": "ticket_type"},
+        related_name='+',
+        verbose_name=_("Ticket Category")
+    )
+
+    event = models.ForeignKey(
+        Event,
+        on_delete=models.CASCADE,
+        verbose_name=_("Event"),
+        related_name="ticket_types"
+    )
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        verbose_name=_("Price")
+    )
+
+    total_tickets = models.PositiveIntegerField(
+        verbose_name=_("Total Tickets")
+    )
+
+    available_tickets = models.PositiveIntegerField(
+        verbose_name=_("Available Tickets")
+    )
+
+    class Meta:
+        verbose_name = _("Ticket Category Price")
+        verbose_name_plural = _("Ticket Category Prices")
+        db_table = "ticket_types"
+
     def save(self, *args, **kwargs):
-        """Set available seats to capacity when creating an event for the first time."""
+        """Set available tickets to total tickets when
+        creating an event for the first time only."""
         # Only set if event is being created (not updated)
         if self._state.adding:
-            self.available_seats = self.capacity
+            self.available_tickets = self.total_tickets
         super().save(*args, **kwargs)
 
-    def update_available_seats(self, seats: int):
-        """Update the available seats for the event."""
-        self.available_seats -= seats
+    def update_available_tickets(self, quantity: int):
+        self.available_tickets -= quantity
         self.save()
 
     def __str__(self) -> str:
-        return self.name
+        return f"{self.price} for {self.category.name} ticket"
 
 
 class Reservation(AbstractBaseModel):
@@ -80,10 +130,21 @@ class Reservation(AbstractBaseModel):
         Event, related_name="reservations", on_delete=models.CASCADE
     )
 
+    ticket_type = models.ForeignKey(
+        TicketType,
+        on_delete=models.CASCADE,
+        related_name="reservation",
+        verbose_name=_("Ticket Type")
+    )
+
+    ticket_quantity = models.PositiveIntegerField(
+        verbose_name=_("Ticket Quantity")
+    )
+
     code = models.CharField(
-        verbose_name=_("Code"),
+        unique=True,
         max_length=100,
-        unique=True
+        verbose_name=_("Code")
     )
 
     reserved_date = models.DateTimeField(
@@ -95,7 +156,7 @@ class Reservation(AbstractBaseModel):
         on_delete=models.RESTRICT,
         limit_choices_to={"type": "reservation_status"},
         related_name="+",
-        verbose_name=_("Status")     
+        verbose_name=_("Status")
     )
 
     payment_status = models.ForeignKey(
@@ -108,54 +169,26 @@ class Reservation(AbstractBaseModel):
     )
 
     class Meta:
-        ordering = ["-created_at"]
         verbose_name = _("Reservation")
         verbose_name_plural = _("Reservations")
         db_table = "reservations"
 
     def __str__(self) -> str:
-        return f"Reservation for {self.user} at {self.event.name}"
-
-
-class TicketCategoryPrice(AbstractBaseModel):
-    category = models.ForeignKey(
-        DataLookup,
-        on_delete=models.CASCADE,
-        limit_choices_to={"type": "ticket_category_type"},
-        related_name='+',
-        verbose_name=_("Ticket Category")
-    )
-
-    price = models.DecimalField(
-        max_digits=10,
-        decimal_places=2,
-        verbose_name=_("Price")
-    )
-
-    class Meta:
-        ordering = ["-created_at"]
-        verbose_name = _("Ticket Category Price")
-        verbose_name_plural = _("Ticket Category Prices")
-        db_table = "ticket_category_price"
-
-    def __str__(self) -> str:
-        return f"{self.price} for {self.category.name} ticket"
+        return f"Reservation for {self.user.email} at {self.event.name}"
 
 
 class Ticket(AbstractBaseModel):
-    event = models.ForeignKey(
-        Event,
-        on_delete=models.CASCADE,
+    reservation = models.ForeignKey(
+        Reservation,
+        on_delete=models.PROTECT,
         related_name="tickets",
-        verbose_name=_("Event"),
+        verbose_name=_("Reservation"),
     )
 
     ticket_number = models.CharField(
-        verbose_name=_("Ticket Number"), max_length=255, unique=True
-    )
-
-    seat_number = models.CharField(
-        verbose_name=_("Seat Number"), max_length=50, blank=True, null=True
+        verbose_name=_("Ticket Number"),
+        max_length=255,
+        unique=True
     )
 
     status = models.ForeignKey(
@@ -173,45 +206,19 @@ class Ticket(AbstractBaseModel):
     )
 
     class Meta:
-        ordering = ["-created_at"]
         verbose_name = _("Ticket")
         verbose_name_plural = _("Tickets")
         db_table = "tickets"
 
     def __str__(self) -> str:
-        return f"{self.event.name} - {self.ticket_number}"
+        return f"{self.ticket_number}"
 
 
-class ReservationItem(AbstractBaseModel):
-    reservation = models.ForeignKey(
-        Reservation, related_name="reservation_items", on_delete=models.CASCADE, verbose_name=_("Reservation")
-    )
-
-    ticket = models.ForeignKey(
-        Ticket, related_name="reservation_items", on_delete=models.CASCADE, verbose_name=_("Ticket")
-    )
-
-    class Meta:
-        ordering = ["-created_at"]
-        verbose_name = _("Reservation Item")
-        verbose_name_plural = _("Reservation Items")
-        db_table = "reservation_items"
-        constraints = [
-            models.UniqueConstraint(
-                fields=["reservation", "ticket"], name="unique_ticket_per_reservation"
-            )
-        ]
-
-    def __str__(self) -> str:
-        return f"Ticket {self.ticket.ticket_number} for {self.reservation.event.name}"
-
-
-# ? Can we change this name to PaymentTransaction?
-class Payment(AbstractBaseModel):
+class Transaction(AbstractBaseModel):
     reservation = models.OneToOneField(
         Reservation,
         on_delete=models.CASCADE,
-        related_name="payment",
+        related_name="transaction",
         verbose_name=_("Reservation")
     )
 
@@ -221,23 +228,22 @@ class Payment(AbstractBaseModel):
         decimal_places=2
     )
 
-    payment_date = models.DateTimeField(
-        verbose_name=_("Payment Date"),
+    transaction_date = models.DateTimeField(
+        verbose_name=_("Transaction Date"),
         help_text=_("Format: YYYY-MM-DD HH:MM:SS"),
         auto_now_add=True,
     )
 
-    # ? How will it be? Will it be a dropdown or a text field
+    # * Will be auto tracked from user activity
     payment_method = models.CharField(
         verbose_name=_("Payment Method"),
         max_length=50
     )
 
     class Meta:
-        ordering = ["-created_at"]
-        verbose_name = _("Payment")
-        verbose_name_plural = _("Payments")
-        db_table = "payments"
+        verbose_name = _("transaction")
+        verbose_name_plural = _("transactions")
+        db_table = "transactions"
 
     def __str__(self) -> str:
-        return f"Payment for {self.reservation} - {self.amount}"
+        return f"Payment transaction for {self.reservation} - {self.amount}"
