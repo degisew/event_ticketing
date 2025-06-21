6eecf7bdc26f5c0e4e839d2039891f5ccb19e637
Degisew <jegisew21@gmail.com>
2025-06-21 14:29:20 +0300

    fix: user email and ticket attachment

--- apps/event/tasks.py
+++ apps/event/tasks.py
@@ -5,24 +5,30 @@ from celery import shared_task
 
 
 @shared_task
-def send_ticket_email(tickets_data, recipient_email):
-    subject = "Your Event Tickets"
-    message = "Here are your tickets."
+def send_ticket_email(tickets_data, recipient_email, event):
+    subject = f"Your Tickets for {event}"
+    message = (
+        f"Dear {recipient_email[0]},\n\n"
+        f"Thank you for your reservation for the {event} event.\n"
+        f"We've attached your tickets below as a QR code.\n\n"
+        "We look forward to seeing you at the event!\n\n"
+        "Best regards,\n"
+        "The Team"
+    )
 
     email = EmailMessage(
         subject, message, settings.DEFAULT_FROM_EMAIL, to=recipient_email
     )
 
     # Loop through the tickets and generate a QR code for each
-    for ticket in tickets_data:
-        ticket_info = f"Ticket ID: {ticket['ticket_number']}, Seat: {ticket['seat_number']}"
-        print("????", ticket_info)
+    for ticket_number in tickets_data:
+        ticket_info = f"Ticket ID: {ticket_number}"
 
         # Generate the QR code for each ticket
         qr_image = generate_qr_code(ticket_info)
 
         # Attach QR Code to Email
-        email.attach(f"ticket_qr_{ticket['ticket_number']}.png", qr_image.getvalue(), "image/png")
+        email.attach(f"ticket_qr_{ticket_number}.png", qr_image.getvalue(), "image/png")
 
     # Send the email
     email.send()
