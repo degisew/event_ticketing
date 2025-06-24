61ac070132250647383f268f4860d3bf0f6842c0
Degisew <jegisew21@gmail.com>
2025-06-24 23:29:47 +0300

    conf: configured django-silk for profiling

--- config/settings/base.py
+++ config/settings/base.py
@@ -37,7 +37,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG", default=True)
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS", default=["*"])
+ALLOWED_HOSTS = env("ALLOWED_HOSTS", default=["*"], cast=list)
 
 WSGI_APPLICATION = "config.wsgi.application"
 
--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,12 +1,31 @@
 import socket
 from config.settings.base import *  # noqa F403
-from config.settings.base import INSTALLED_APPS, MIDDLEWARE
+from config.settings.base import INSTALLED_APPS, MIDDLEWARE, ALLOWED_HOSTS
 
-INSTALLED_APPS += ["drf_spectacular", "debug_toolbar"]
+INSTALLED_APPS += ["drf_spectacular", "silk", "debug_toolbar"]
 
 MIDDLEWARE += [
+    'silk.middleware.SilkyMiddleware',
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
+
+ALLOWED_HOSTS += ["127.0.0.1", "localhost"]
+
+
+def show_toolbar(request):
+    return True
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": show_toolbar,
+    "DISABLE_PANELS": [
+        "debug_toolbar.panels.redirects.RedirectsPanel",
+        # Disable profiling panel due to an issue with Python 3.12:
+        # https://github.com/jazzband/django-debug-toolbar/issues/1875
+        "debug_toolbar.panels.profiling.ProfilingPanel",
+    ],
+    "SHOW_TEMPLATE_CONTEXT": True,
+}
 
 
 # Since we're in Docker, we do this to match the machine address of Docker
