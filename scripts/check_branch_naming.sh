64e2705a3b4890cbe12568f4389dd79ff33c4e11
Degisew <jegisew21@gmail.com>
2025-06-25 18:20:41 +0300

    fix(script): added perf flag

--- scripts/check_branch_naming.sh
+++ scripts/check_branch_naming.sh
@@ -2,7 +2,7 @@
 
 branch_name=$(git rev-parse --abbrev-ref HEAD)
 
-pattern="^(feature|bugfix|hotfix|refactor|chore|docs)/[a-z0-9\-_]+$"
+pattern="^(feature|bugfix|hotfix|refactor|test|perf|chore|docs)/[a-z0-9\-_]+$"
 
 if ! echo "$branch_name" | grep -qE "$pattern"; then
   echo "Branch name '$branch_name' is invalid!"
