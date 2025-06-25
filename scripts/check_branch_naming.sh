4a766d37761a83a635a7118eb1a147b0b23d83b9
Degisew <jegisew21@gmail.com>
2025-06-25 21:25:11 +0300

    ref(scripts): colorizing shell messages

--- scripts/check_branch_naming.sh
+++ scripts/check_branch_naming.sh
@@ -5,7 +5,9 @@ branch_name=$(git rev-parse --abbrev-ref HEAD)
 pattern="^(feature|bugfix|hotfix|refactor|test|perf|chore|docs)/[a-z0-9\-_]+$"
 
 if ! echo "$branch_name" | grep -qE "$pattern"; then
-  echo "Branch name '$branch_name' is invalid!"
-  echo "Use: feature/my-feature-name"
+  echo "Branch name doesn't follow Conventional branch name format"\n
+  echo "Format: <type>/branch-name"\n
+  echo -e "\e[36mExample:  feature/feature-branch-name\e[0m" \n \n
+  echo -e "\e[36mAvailable tpes are [feature, bugfix, hotfix, refactor, test, perf, chore, docs]\e[0m"
   exit 1
 fi
--- scripts/check_commit_message.sh
+++ scripts/check_commit_message.sh
@@ -6,8 +6,8 @@ commit_msg=$(cat "$commit_msg_file")
 pattern="^(feat|fix|docs|style|ref|conf|test|chore|perf)(\([a-z0-9\-]+\))?: .+"
 
 if ! echo "$commit_msg" | grep -qE "$pattern"; then
-  echo "Commit message doesn't follow Conventional Commit format"
-  echo "Format: <type>(optional-scope): message"
-  echo "Example: feat(auth): add token login"
+  echo "Commit message doesn't follow Conventional Commit format\n"
+  echo "Format: <type>(optional-scope): message \n"
+  echo -e "\e[36mExample: feat(auth): add token login\e[0m"
   exit 1
 fi
