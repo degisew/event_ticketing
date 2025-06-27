fd552e429dd8c40ff2f7e8564c73d2e9a36e5312
Degisew <jegisew21@gmail.com>
2025-06-28 01:26:40 +0300

    test(load): add more load test for ticket_type and reservation endpoints

--- locust/get_events.py
+++ locust/get_events.py
@@ -3,8 +3,32 @@ from locust import HttpUser, task, between
 
 
 class EventUser(HttpUser):
+
     wait_time = between(1, 2)
-    # url_prefix = "/api/v1/"
+
+    EVENT_IDS = [
+        "9cfac9c3-2568-4593-8bd1-c8d6daa09ad7"
+    ]
+
+    def on_start(self) -> None:
+        response = self.client.post(
+            "/api/v1/auth/login/",
+            json={
+                "email": "admin@gmail.com",
+                "password": 1234
+            }
+        )
+
+        token = response.json().get("access")
+        if token:
+            self.client.headers.update({
+                "Authorization": f"Bearer {token}"
+            })
+
+        response = self.client.get("/api/v1/event/events/")
+        data = response.json()
+        events = data.get("results", [])
+        self.event_ids = [event["id"] for event in events]
 
     @task(3)
     def browse_events(self):
@@ -12,12 +36,14 @@ class EventUser(HttpUser):
 
     @task(1)
     def browse_event_detail(self):
-        id = random.choice(self.event_ids)
+        id = random.choice(self.EVENT_IDS)
 
         self.client.get(f"/api/v1/event/events/{id}", name="/event_detail")
 
-    def on_start(self) -> None:
-        response = self.client.get("/api/v1/event/events/")
-        data = response.json()
-        events = data.get("results", [])
-        self.event_ids = [event["id"] for event in events]
+    @task
+    def browse_ticket_types(self):
+        self.client.get("/api/v1/event/ticket_types/", name="/ticket_type")
+
+    @task
+    def browse_reservations(self):
+        self.client.get("/api/v1/event/reservations/", name="reservations")
