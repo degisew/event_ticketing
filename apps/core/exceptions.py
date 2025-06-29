97654fdc131b71558de59a421eb555edf4bd47bc
Degisew <jegisew21@gmail.com>
2025-06-29 17:16:01 +0300

    ref(api): Moved common project level exceptions to core app

---
+++ apps/core/exceptions.py
@@ -0,0 +1,25 @@
+from rest_framework.exceptions import APIException
+
+
+class ServiceBaseException(APIException):
+    status_code = 500
+    default_detail = "An error occurred in the ticketing system"
+    default_code = "TICKETING_ERROR"
+
+
+class SerializationError(APIException):
+    status_code = 500
+    default_detail = "Object Serialization Error"
+    default_code = "SERIALIZATION_ERROR"
+
+
+class DataIntegrityError(ServiceBaseException):
+    status_code = 500
+    default_detail = "A system error occurred"
+    default_code = "DATA_INTEGRITY_ERROR"
+
+
+class ExternalServiceError(ServiceBaseException):
+    status_code = 502
+    default_detail = "External service temporarily unavailable"
+    default_code = "EXTERNAL_SERVICE_ERROR"
